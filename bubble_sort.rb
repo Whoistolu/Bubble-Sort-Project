c5da6f33992b52e2849ab448cd216e4e1e77a5c5
Toluwashe Ajise <digittolu25@gmail.com>
2021-06-13 22:52:09 +0100

    add algorithm for the project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,3 +15,21 @@ def bubble_sort (array)
       end
       array
 end
+
+def bubble_sort_by(array)
+    not_sorted = true
+    while not_sorted
+      i = 0
+      not_sorted = false
+      while i < array.length - 1
+        if (yield array[i], array[i + 1]) >= 0
+          store = array[i]
+          array[i] = array[i + 1]
+          array[i + 1] = store
+          not_sorted = true
+        end
+        i += 1
+      end
+    end
+    array
+  end
